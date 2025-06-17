80dadb02f66c38d889015bd5ba26be3f44d91ad8
Andriel Marafigo <165610954+Andrielmarafigo@users.noreply.github.com>
2025-06-17 19:55:43 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -8,7 +8,5 @@ document.addEventListener('DOMContentLoaded', function () {
     mensagem.style.color = 'green';
     mensagem.textContent = '✅ Formulário enviado com sucesso!';
 
-    // Aqui você pode adicionar lógica para enviar os dados via AJAX ou resetar o formulário:
-    // form.reset(); // Descomente se quiser limpar o formulário após o envio
   });
 });
